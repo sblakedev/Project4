dc18a31f09597ed191091d1d04d232ece14108ec
Suzanne Blake <suzanne.blake22@gmail.com>
2022-10-22 16:31:56 +0000

    Start building Booking model

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+  
+class Booking(models.Model):
+    Booking_Date =
+    Booking_Time = 
+    No_of_guests =
+    Booking_event = 
