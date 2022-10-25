64bb5c30999a76b92252169eac1dc13c04575667
Suzanne Blake <suzanne.blake22@gmail.com>
2022-10-25 19:37:33 +0000

    Add Create Account model

--- bookings/models.py
+++ bookings/models.py
@@ -63,9 +63,9 @@ TITLE = (
 
 class Booking(models.Model):
     guest = models.ForeignKey(User, on_delete=models.CASCADE, related_name="guest_bookings")
-    name = models.CharField(max_length=50, null=True, blank=True)
+    name = models.CharField(max_length=50, null=True, blank=False)
     email_address = models.EmailField(null=True, blank=True),
-    phone = models.IntegerField(null=True, blank=True)
+    phone = models.IntegerField(null=True, blank=False, min_length=7)
     booking_date = models.DateField(auto_now=True, blank=False)
     booking_time = models.TimeField(auto_now=True, choices=TIME, blank=False)
     no_of_guests = models.CharField(max_length=2, choices=NO_OF_GUESTS, blank=False)
@@ -75,4 +75,8 @@ class Booking(models.Model):
         return self.name
 
 class CreateAccount(models.Models):
-    title = models.Char
+    title = models.CharField(max_length=4, blank=False)
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    email_address = models.EmailField(blank=True)
+    phone_number = models.IntegerField(blank=False)
