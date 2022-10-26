60781f6f925410cc6d4eb4ca5ecd4914e529f0b4
Suzanne Blake <suzanne.blake22@gmail.com>
2022-10-26 18:26:24 +0000

    Add booking admin class and decorator

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Booking
 
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    model = Booking
+
+
 # Register your models here.
-admin.site.register(Booking)
+
