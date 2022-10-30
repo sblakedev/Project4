b737abfb0d5162abb201c778637c5c67deda14a9
Suzanne Blake <suzanne.blake22@gmail.com>
2022-10-30 15:35:45 +0000

    Add bookings list to urls

--- saoirsesburgers/urls.py
+++ saoirsesburgers/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('bookings_list', views.all_bookings, name="list-bookings"),
+    
 ]
 
-path('bookings_list', views.all_bookings, name="list-bookings"),
