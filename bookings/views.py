63a860516f44dc10c636d77fc045811c06a5fce2
Suzanne Blake <suzanne.blake22@gmail.com>
2022-10-30 17:19:16 +0000

    Add basic page views

--- bookings/views.py
+++ bookings/views.py
@@ -4,28 +4,49 @@ from django.views import generic
 
 # Create your views here.
 
-# Home Page
-"""Home Page View"""
+
 class HomePage(generic.DetailView):
+    """
+    Renders Home Page View
+    """
+
     template_name = "index.html"
-    
+
     def get(self, request):
         return render(request, 'index.html')
 
-# Menu Page
-"""Menu Page View"""
-class MenuPage(View)
 
-# Book a Table Page
-"""Book a Table Page View"
-class Booking
+class MenuPage(View):
+    """
+    Renders Menu Page View
+    """
+
+    def get(self, request):
+        return render(Request, 'menu.html')
 
 
-class BookingsList(generic.ListView):
-    model = Booking
-    queryset = Booking.objects.order_by('booking_date', 'booking_time')
-    template_name = 'bookings_list.html'
-    
-def all_bookings(request):
-    bookings_list = Booking.objects.all()
-    return render(request, 'bookings_list.html' {'bookings_list': bookings_list})
+class ContactPage(View):
+    """
+    Renders Contact Page View
+    """
+
+    def get(self, request):
+        return render(Request, 'contact.html')
+
+
+class LogInPage(View):
+    """
+    Renders Log In Page View
+    """
+
+    def get(self, request):
+        return render(Request, 'log_in.html')
+
+
+class BookPage(View):
+    """
+    Renders Booking Page View
+    """
+
+    def get(self, request):
+        return render(Request, 'book.html')
