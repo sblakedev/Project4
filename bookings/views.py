45490a8e1ef796b290b6ec39b4b1925b2fc614f2
Suzanne Blake <suzanne.blake22@gmail.com>
2022-10-30 15:54:07 +0000

    Add navbar and footer

--- bookings/views.py
+++ bookings/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
-from django.views import generic
-from .models import Booking
+# from django.shortcuts import render
+# from django.views import generic
+# from .models import Booking
 
-# Create your views here.
-class BookingsList(generic.ListView):
-    model = Booking
-    queryset = Booking.objects.order_by('booking_date', 'booking_time')
-    template_name = 'bookings_list.html'
+# # Create your views here.
+# class BookingsList(generic.ListView):
+#     model = Booking
+#     queryset = Booking.objects.order_by('booking_date', 'booking_time')
+#     template_name = 'bookings_list.html'
     
-def all_bookings(request):
-    bookings_list = Booking.objects.all()
-    return render(request, 'bookings_list.html' {'bookings_list': bookings_list})
+# def all_bookings(request):
+#     bookings_list = Booking.objects.all()
+#     return render(request, 'bookings_list.html' {'bookings_list': bookings_list})
