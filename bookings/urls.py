9c67dbe23ce43b4c9719b4e7d4df71f9c330de09
Suzanne Blake <suzanne.blake22@gmail.com>
2022-11-03 20:05:47 +0000

    Change views.Home.as_view to views.home

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home'),
+    path('', views.home, name='home'),
     path('', views.Book.as_view(), name='book'),
     path('', views.About.as_view(), name='about'),
     path('', views.Menu.as_view(), name='menu'),
