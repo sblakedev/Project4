aa75799ab5b5f4a8007b39a75eaa945f83b117f6
Suzanne Blake <suzanne.blake22@gmail.com>
2022-11-03 19:50:04 +0000

    Add paths for Home, book, about and menu

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.BookPage.as_view(), name='book')
+    path('', views.Home.as_view(), name='home'),
+    path('', views.Book.as_view(), name='book'),
+    path('', views.About.as_view(), name='about'),
+    path('', views.Menu.as_view(), name='menu'),
 ]
