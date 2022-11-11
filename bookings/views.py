b9dc727c6e994b632aeb81a5a9635c401d4895c8
Suzanne Blake <suzanne.blake22@gmail.com>
2022-11-11 10:48:06 +0000

    Change brackets to square barckets for choices

--- bookings/views.py
+++ bookings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, reverse, redirect
 from django.contrib import messages
 from django.views import generic, View
 from django.views.generic.edit import FormView
+from django.shortcuts import get_object_or_404
 from .models import Booking
 from .forms import BookingForm
 
@@ -22,16 +23,17 @@ def book_page(request):
     Renders Booking Page View
     """
     if request.method == 'POST':
-        form = BookingForm(request.POST, request.FILES)
+        form = BookingForm(request.POST)
         if form.is_valid():
-            booking = form.save(commit=False)
-            booking.user = request.user
-            booking.save()
+            booking_form = form.save(commit=False)
+            booking_form.user = request.user
+            booking_form.save()
             messages.success(request, 'Booking is confirmed')
             return redirect('my_bookings')
         else:
             messages.error(
                 request, 'Invalid, incorrect info or double booking')
+            print(form.errors)
 
     form = BookingForm()
     context = {
@@ -56,14 +58,13 @@ def my_bookings(request):
     else:
         return redirect('../accounts/signup')
 
-    
+
 def edit_bookings(request):
-    booking = get_object_or_404(Booking, id=booking_id)
+    booking = get_object_or_404(Booking)
     if request.method == 'POST':
         form = BookingForm(request.POST, instance=booking)
         if form.is_valid():
             form.save()
-            booking.save()
             messages.success(request, 'Your booking has been updated')
             return redirect('my_bookings')
         else:
